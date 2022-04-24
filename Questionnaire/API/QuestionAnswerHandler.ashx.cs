61c9347ba14340941370ba1259f8b9f580072e70
KakuYuki <yuki515396@yahoo.com.tw>
2022-04-24 11:09:56 +0800

    QuestionAnswerHandler完成95%

--- Questionnaire/API/QuestionAnswerHandler.ashx.cs
+++ Questionnaire/API/QuestionAnswerHandler.ashx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuestionModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,8 +14,52 @@ namespace Questionnaire.API
 
         public void ProcessRequest(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
+            if (string.Compare("POST", context.Request.HttpMethod) == 0 &&
+                 Guid.TryParse(context.Request.QueryString["quesID"], out Guid quesID))
+            {
+                string name = context.Request.Form["Name"];
+                string phone = context.Request.Form["Phone"];
+                string email = context.Request.Form["Email"];
+                string age = context.Request.Form["Age"];
+
+                AccountInfoModel accountInfoModel = new AccountInfoModel()
+                {
+                    AccountID = Guid.NewGuid(),
+                    Name = name,
+                    Phone = phone,
+                    Age = age,
+                    Email = email,
+                    quesID = quesID,
+                };
+                HttpContext.Current.Session["questionanswer"] = accountInfoModel;
+                string quesans = context.Request.Form["Answer"];
+                if (string.IsNullOrWhiteSpace(quesans))
+                {
+                    context.Response.ContentType = "text/plain";
+                    context.Response.Write("noAnswer");
+                    return;
+                }
+
+                string[] AnswerArr = quesans.Trim().Split(' ');
+                List<QuestionAnswerModel> questionAnswers = new List<QuestionAnswerModel>();
+                foreach (string item in AnswerArr)
+                {
+                    string[] answer = item.Split(' ');
+
+                    QuestionAnswerModel questionAnswerModel = new QuestionAnswerModel()
+                    {
+                        AccountID = Guid.NewGuid(),
+                        quesID = quesID,
+                        quesNumber = answer.Length,
+                        Answer = AnswerArr[1],
+                    };
+                    questionAnswers.Add(questionAnswerModel);
+                }
+                HttpContext.Current.Session["peopleAnswer"] = questionAnswers;
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("Questionnaire Complete");
+            }
+
         }
 
         public bool IsReusable
